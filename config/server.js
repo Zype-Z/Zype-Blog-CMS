cb1b1678b0192cd08f73f55cef6c9bb0e6a74ef0
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-06-21 16:40:36 +0530

    Initial Strapi Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b03702ae9a81593d1a0045b6833f5f19'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '03f1ad1e-c652-485b-8050-ea09e20b8bf4'
+};
