67d34ac058a3bde77e11e37953fd2d90239dcfc7
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-06-22 15:25:09 +0530

    Last Fixup

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
-        username: env('DATABASE_USERNAME', process.env.DATABASE_USER),
-        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASS),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USER'),
+        password: env('DATABASE_PASS'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
